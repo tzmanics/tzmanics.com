45ba98db32ddc11f6c7e7e74e5cb82a54d2c7e35
tara manicsic <tzmanics@gmail.com>
2014-02-04 15:48:24 -0500

    portfolio page file merge

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$(".main").onepage_scroll({
+	sectionContainer: "section",
+	responsiveFallback: 600
+	});
+	$('#camera_wrap_2').camera({
+	});
+
+	$('#camera_wrap_3').camera({
+	});
+
+	$('#camera_wrap_4').camera({
+	});
+	$('#camera_wrap_5').camera({
+	});
+});
