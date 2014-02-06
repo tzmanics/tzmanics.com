a1752d14267efe53ec6025e4076cb469f489c6a4
tara manicsic <tzmanics@gmail.com>
2014-02-06 18:06:36 -0500

    -one page scroll. +function for scrollTop & file organization

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$('section').height(function(){
+	    return $(window).height();
+	});
+	$('#camera_wrap_2').camera({
+	});
+	$('#camera_wrap_3').camera({
+	});
+	$('#camera_wrap_4').camera({
+	});
+	$('#camera_wrap_5').camera({
+	});
+	$('#info').on('click',function(){
+		event.preventDefault();
+		$('html').animate({ scrollTop: 0 }, 1500);
+	});
+	$('#web').on('click',function(){
+		event.preventDefault();
+		$("html, body").animate({
+			scrollTop:$("#webSection").offset().top}, 1500);
+	})
+	$('#mobile').on('click',function(){
+		event.preventDefault();
+		$("html, body").animate({
+			scrollTop:$("#mobileSection").offset().top}, 1500);
+	})
+	$('#design').on('click',function(){
+		event.preventDefault();
+		$("html, body").animate({
+			scrollTop:$("#designSection").offset().top}, 1500);
+	})
+	$('#art').on('click',function(){
+		event.preventDefault();
+		$("html, body").animate({
+			scrollTop:$("#artSection").offset().top}, 1500);
+	})
+});
